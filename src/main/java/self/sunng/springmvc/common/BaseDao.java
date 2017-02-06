ed47670cf3834b2670faa722da4ea3c174248283
孙晓冬 <47142487@qq.com>
2017-02-06 16:46:41 +0800

    update jar version

--- src/main/java/self/sunng/springmvc/common/BaseDao.java
+++ src/main/java/self/sunng/springmvc/common/BaseDao.java
@@ -7,16 +7,16 @@ import javax.annotation.Resource;
 
 public class BaseDao {
 
-	protected final static int DEFAULT_PAGE_SIZE = 15;
+    protected final static int DEFAULT_PAGE_SIZE = 15;
 
-	@Resource(name = "sqlSessionTemplate")
-	protected SqlSessionTemplate sqlSessionReader;
+    @Resource(name = "sqlSessionTemplate")
+    protected SqlSessionTemplate sqlSessionReader;
 
-	@Resource(name = "sqlSessionTemplate")
-	protected SqlSessionTemplate sqlSessionWriter;
+    @Resource(name = "sqlSessionTemplate")
+    protected SqlSessionTemplate sqlSessionWriter;
 
-	protected int getStartIndex(int pageNum, int pageSize) {
-		return (pageNum - 1) * pageSize;
-	}
+    protected int getStartIndex(int pageNum, int pageSize) {
+        return (pageNum - 1) * pageSize;
+    }
 
 }
--- src/main/java/self/sunng/springmvc/common/interceptor/AuthorizationInterceptor.java
+++ src/main/java/self/sunng/springmvc/common/interceptor/AuthorizationInterceptor.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Method;
 @Component
 public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
 
+    @SuppressWarnings("Duplicates")
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) throws Exception {
         //如果不是映射到方法直接通过
